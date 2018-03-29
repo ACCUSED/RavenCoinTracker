14264f6f8273603b232e076cc392f8711d62de42
Jake Crystal <jake0425@gmail.com>
2018-03-28 22:07:01 -0400

    Cleaner
    Next is to fix the layout. MATERIAL

--- app/src/main/java/com/warpigs_online/ravencointracker/MainActivity.java
+++ app/src/main/java/com/warpigs_online/ravencointracker/MainActivity.java
@@ -1,20 +1,24 @@
 package com.warpigs_online.ravencointracker;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,11 +56,11 @@ public class MainActivity extends AppCompatActivity {
 
                 reader = new BufferedReader(new InputStreamReader(stream));
 
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
+                StringBuilder buffer = new StringBuilder();
+                String line;
 
                 while ((line = reader.readLine()) != null) {
-                    buffer.append(line+"\n");
+                    buffer.append(line).append("\n");
                     Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
 
                 }
@@ -81,10 +85,56 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
 
+        @SuppressLint("SetTextI18n")
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            txtJson.setText(result);
+
+            try {
+                JSONArray jsonarray = new JSONArray(result);
+
+                // Only one array so ZERO!
+                JSONObject jsonobject = jsonarray.getJSONObject(0);
+
+                String id       = jsonobject.getString("id");
+                int rank    = jsonobject.getInt("rank");
+                double price_usd    = jsonobject.getDouble("price_usd");
+                double price_btc    = jsonobject.getDouble("price_btc");
+                int tfh_volume_usd    = jsonobject.getInt("24h_volume_usd");
+                int market_cap_usd    = jsonobject.getInt("market_cap_usd");
+                int total_supply    = jsonobject.getInt("total_supply");
+                int percent_change_1h    = jsonobject.getInt("percent_change_1h");
+                int percent_change_24h    = jsonobject.getInt("percent_change_24h");
+                int percent_change_7d    = jsonobject.getInt("percent_change_7d");
+                long last_updated    = jsonobject.getLong("last_updated");
+
+                // convert seconds to milliseconds
+                Date date = new java.util.Date(last_updated*1000L);
+                // the format of your date
+                SimpleDateFormat sdf =  new SimpleDateFormat("MMM dd, YYYY hh:mm:ss a",
+                        java.util.Locale.getDefault());
+                String formattedDate = sdf.format(date);
+                System.out.println(formattedDate);
+
+                txtJson.setText("Rank: " + rank + "\n" +
+                                "Price: $" + price_usd + "\n" +
+                                "Price BTC: " + price_btc + " \n" +
+                                "24H Volume: $" + tfh_volume_usd + "\n" +
+                                "Market Cap USD: $" + market_cap_usd + "\n" +
+                                "Supply: " + total_supply + "/ 21,000,000,000" + "\n" +
+                                "Change 1H: " + percent_change_1h + "%" + "\n" +
+                                "Change 24H: " + percent_change_24h + "%" + "\n" +
+                                "Change 7D: " + percent_change_7d + "%" + "\n" +
+                                "Last Updated: " + formattedDate);
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+
+
+
+
+
+
             Log.d("INFO", "msg: " + result);
         }
     }
